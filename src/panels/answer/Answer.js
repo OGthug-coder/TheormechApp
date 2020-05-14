ceaf9f90e73e8e72a3f3c81728686a1f9c441fa3
03regionkruto <almatinez101@yandex.ru>
2020-05-15 00:38:12 +0700

    answer done

---
+++ src/panels/answer/Answer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import s from "./Answer.module.css";
+
+class Answer extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div className={s.answer_block}>
+                <div className={s.head}>
+                    <div className={s.question_label}>
+                        <img src={require("../../img/answer/question.png")} width="24" height="24" />
+                    </div>
+                    <a href="#" className={s.exit}>
+                        &#10006;
+                    </a>
+                </div>
+                <div className={s.question}>В каком случае относительная скорость движущихся автомобилей максимальна?</div>
+                <div className={s.ans_title}>Правильный ответ:</div>
+                <div className={s.ans}>Автомобили движутся навстречу друг другу</div>
+                <div className={s.explanation_title}>Почему?</div>
+                <div className={s.explanation}>Потому что lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+            </div>
+        )
+    }
+}
+
+export default Answer;
--- src/panels/preview/Preview.js
+++ src/panels/preview/Preview.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ModalFragment from "./fragments/ModalFragment";
+import Answer from "../answer/Answer";
 
 import s from "./Preview.module.css";
 
@@ -17,7 +18,9 @@ class Preview extends React.Component{
                     <img src={require("../../img/preview/ic_preview_background.svg")} alt={"background"} />
                 </div>
                 <ModalFragment />
+                <Answer />
             </section>
+            
         )
     }
 }
