77a8ba331e6fb5d44ab9a886819c585fa7a641a7
jhoysbou <neeroll21@gmail.com>
2020-08-08 16:33:45 +0300

    backHeader implemented

---
+++ src/panels/stickershop/StickerShop.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import BackHeader from "../../common/components/backheader/BackHeader";
+
+class StickerShop extends React.Component{
+    render() {
+        return (
+            <>
+                <BackHeader />
+            </>
+        );
+    }
+}
+
+export default StickerShop;
