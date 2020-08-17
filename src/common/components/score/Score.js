8e1b63f7cafb221c213df4ee1854a286aac523b9
jhoysbou <neeroll21@gmail.com>
2020-08-17 22:15:55 +0300

    zoom off

--- src/common/components/score/Score.js
+++ src/common/components/score/Score.js
@@ -13,7 +13,7 @@ class Score extends React.Component {
     render() {
         return (
             <div className={s.container}>
-                <img src={require("../../../img/profile/ic_score.svg")}/>
+                <img src={require("../../../img/profile/ic_score.svg")} alt={"score"}/>
                 <span className={s.score}>{this.state.score}</span>
             </div>
         );
