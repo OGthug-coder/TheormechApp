97e7907f437bf9a2073ce276e42f2c27ac0de5b1
jhoysbou <neeroll21@gmail.com>
2020-08-21 19:50:11 +0300

    delay increased

--- src/panels/preview/Preview.js
+++ src/panels/preview/Preview.js
@@ -59,7 +59,7 @@ class Preview extends React.Component {
 
     onSwipeEnd = (position, event) => {
         if (!isUndefined(this.state.answerWindow)) {
-            setTimeout(this.hideAnswerWindow, 35);
+            setTimeout(this.hideAnswerWindow, 50);
         }
     };
 
