b7a39f93282964b4df54bf322813686339499938
jhoysbou <neeroll21@gmail.com>
2020-09-08 18:30:34 +0300

    questionListItems are done

--- src/panels/admin/questioncreation/QuestionCreation.js
+++ src/panels/admin/questioncreation/QuestionCreation.js
@@ -1,13 +1,46 @@
 import React from 'react';
 import BackHeader from "../../../common/components/backheader/BackHeader";
+import s from "./QuestionCreation.module.css";
+import QuestionListItem from "./fragments/QuestionListItem";
 
 class QuestionCreation extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    onAddQuestionClick = () => {
+        console.log("onAddQuestionClick")
+    };
+
+    onEditQuestionClick = () => {
+        console.log("onEditQuestionClick")
+    };
+
     render() {
         return (
             <>
-                <div>
-                <BackHeader />
-                
+                <BackHeader/>
+                <div className={s.container}>
+                    <div className={s.header}>
+                        Список вопросов
+                        <button onClick={this.onAddQuestionClick}/>
+                    </div>
+                    <div className={s.question_container}>
+                        <div className={s.control}>
+                            <span>Вопрос №1</span>
+                            <button id={1} onClick={this.onEditQuestionClick}/>
+                        </div>
+                        <div className={s.question_item}>
+                            <QuestionListItem question={{text: "Автомобили движутс...чыdsfsdfsd"}}/>
+                        </div>
+                        <div className={s.question_item}>
+                            <QuestionListItem question={{text: "Автомобили движутс..."}}/>
+                        </div>
+                        <div className={s.question_item}>
+                            <QuestionListItem question={{text: "Автомобили движутс..."}}/>
+                        </div>
+
+                    </div>
                 </div>
             </>
         );
---
+++ src/panels/admin/questioncreation/fragments/QuestionListItem.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import s from "./QuestionListItem.module.css";
+
+
+class QuestionListItem extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            question: props.question,
+            onDelete: props.onDelete,
+            onEdit: props. onEdit,
+        };
+    }
+
+    render() {
+        const question = this.state.question;
+        return (
+            <div className={s.question_item}>
+                <button />
+                <div onClick={this.state.onEdit}>
+                    <span>Вариант 1</span>
+                    <span>{question.text}</span>
+                </div>
+                <button onClick={this.state.onEdit}/>
+            </div>
+        )
+    }
+}
+
+export default QuestionListItem;
