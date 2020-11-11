d07f5335d73723a35ee6d1edf4e54d75e815dc82
jhoysbou <neeroll21@gmail.com>
2020-11-12 02:01:21 +0300

    last changes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import bridge from "@vkontakte/vk-bridge";
 import App from "./App";
 
 // Init VK  Mini App
-bridge.send("VKWebAppInit")
+bridge.send("VKWebAppInit", {})
 
 if (navigator.userAgent.indexOf("iPhone") !== -1) {
     bridge.send("VKWebAppSetViewSettings", {
